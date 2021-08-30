20f6a5902410572ab49ebee6fcd92c448378e978
vinod selpol <vinodselpol@gmail.com>
2021-08-30 19:02:48 +0200

    ADD:Slidebar

---
+++ src/TweetBox.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
+
+function TweetBox() {
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+          <input placeholder="what's happening ?" type="text" />
+        </div>
+        <input
+          className="tweetBox__imageInput"
+          placeholder="Enter image URL"
+          type="text"
+        />
+        <Button className="tweetBox__tweetButton"> Tweet</Button>
+      </form>
+    </div>
+  );
+}
+
+export default TweetBox;
